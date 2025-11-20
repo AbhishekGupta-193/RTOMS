5286ba43e80fce3ddc6b58fc77263949c9400d93
ABHISHEK KR. GUPTA <abhishekkgupta193@gmail.com>
2025-11-20 22:48:04 +0530

    Added Spring Project

---
+++ src/main/java/eatclub/rtoms/RtomsBackendApplication.java
@@ -0,0 +1,13 @@
+package eatclub.rtoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtomsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtomsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eatclub/rtoms/RtomsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eatclub.rtoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtomsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
