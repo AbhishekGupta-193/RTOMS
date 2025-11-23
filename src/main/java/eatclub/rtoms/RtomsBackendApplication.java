8518ec19683a450edc7a8be93c414c9cabe4921a
ABHISHEK KR. GUPTA <abhishekkgupta193@gmail.com>
2025-11-23 12:25:32 +0530

    Added Env file to store the secrets

--- src/main/java/eatclub/rtoms/RtomsBackendApplication.java
+++ src/main/java/eatclub/rtoms/RtomsBackendApplication.java
@@ -1,13 +1,15 @@
 package eatclub.rtoms;
-
+import eatclub.rtoms.Config.DotEnvApplicationInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RtomsBackendApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(RtomsBackendApplication.class, args);
+//		SpringApplication.run(RtomsBackendApplication.class, args);
+		SpringApplication application = new SpringApplication(RtomsBackendApplication.class);
+		application.addInitializers(new DotEnvApplicationInitializer());
+		application.run(args);
 	}
 
 }
